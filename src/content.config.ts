868cce849756d4c2c54db6e3ae418b80381ebadb
Kodai Utsunomiya <utsunomiya@mdl.cs.tsukuba.ac.jp>
2026-03-24 17:03:15 +0900

    Fix content collection config

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,7 +2,6 @@ import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
 const blogCollection = defineCollection({
-  type: "content",
   loader: glob({ pattern: "**/*.md", base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
